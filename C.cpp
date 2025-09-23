df7b964080396f55e9d28e420538592c81bfad36
Gui Maia <guilhermemaia05@outlook.com>
2025-09-23 18:05:28 -0300

    Criação da Classe C

---
+++ C.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class C {
+private:
+    string C1;
+    int C2;
+
+public:
+    // Getters e Setters
+    string getC1() {
+        return C1;
+    }
+
+    void setC1(string valor) {
+        C1 = valor;
+    }
+
+    int getC2() {
+        return C2;
+    }
+
+    void setC2(int valor) {
+        C2 = valor;
+    }
+
+    // Métodos
+    void MC1() {
+        cout << "Método MC1 executado" << endl;
+    }
+
+    void MC2() {
+        cout << "Método MC2 executado" << endl;
+    }
+};
+
+int main() {
+
+    return 0;
+}
